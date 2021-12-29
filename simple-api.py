49ce1270476adbdb20847d7359c96fac95b4a68a
MacBook_Trofim <kirill.trofim@gmail.com>
2021-12-29 17:42:10 +0200

    stepFour

--- simple-api.py
+++ simple-api.py
@@ -16,12 +16,9 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-#Как передавать 6 аргументов , а не 4
 
-# , Product_Category_2 = Form(...), Product_Category_3 = Form(...)
 @app.post("/request/")
 def request(Age= Form(...),Gender=Form(...),Occupation = Form(...), Marital_Status = Form(...), Stay_In_Current_City_Years = Form(...),City_Category=Form(...)):
-#  ,"Product_Category_2": [1] ,"Product_Category_3": [2]
     dataframe = pd.DataFrame({"Age":Age,"Gender":[Gender],"Occupation": [Occupation], "Marital_Status": [Marital_Status], "Stay_In_Current_City_Years": [Stay_In_Current_City_Years],"City_Category":[City_Category]})
     model_ridge = pickle.load(open("model.sav", 'rb'))
     result = model_ridge.predict(dataframe).round(2)
